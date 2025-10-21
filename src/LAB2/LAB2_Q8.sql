b2f7ce83d18ae1c14c542be3622180909f93b623
badar24434 <164218215+badar24434@users.noreply.github.com>
2025-10-21 20:44:29 +0800

    Add SQL script to determine the structure and contents of the DEPARTMENTS table

---
+++ src/LAB2/LAB2_Q8.sql
@@ -0,0 +1,12 @@
+-- LAB2 - Question 8
+-- Determine the structure of the DEPARTMENTS table and its contents
+--
+-- SOLUTION EXPLANATION:
+-- Using DESC (DESCRIBE) to show table structure and SELECT to show contents
+-- This helps understand the table design and current data
+
+-- Show the structure of DEPARTMENTS table
+DESC DEPARTMENTS;
+
+-- Show all contents of DEPARTMENTS table
+SELECT * FROM DEPARTMENTS;
